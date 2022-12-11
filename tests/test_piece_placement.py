a305732df22bd54aabfa62c0c92f71821dde0477
jrostek <joanna.rostek@etu.univ-nantes.fr>
2022-12-11 18:37:03 +0100

    Moved player number to setUp

--- tests/test_piece_placement.py
+++ tests/test_piece_placement.py
@@ -9,7 +9,8 @@ class TestPiecePlacement(unittest.TestCase):
     def setUp(self):
         self.n_rows = 6
         self.n_cols = 7
-        self.board = np.zeros((self.n_rows, self.n_cols))
+        self.plr = 1
+        self.board = np.zeros((self.n_rows, self.n_cols), dtype=np.int8)
         self.test_board = self.board.copy()
 
     def test_new_place(self):
@@ -18,8 +19,8 @@ class TestPiecePlacement(unittest.TestCase):
         changes the board layout properly.
         :return:
         """
-        row, col, plr = 2, 2, 1
-        gp.place_piece(self.board, row, col, plr, self.n_rows, self.n_cols)
+        row, col = 2, 2
+        gp.place_piece(self.board, row, col, self.plr, self.n_rows, self.n_cols)
         self.test_board[row, col] = 1
         self.assertIsNone(nptest.assert_array_equal(self.board, self.test_board))
 
@@ -29,11 +30,11 @@ class TestPiecePlacement(unittest.TestCase):
         place a piece in an already taken place.
         :return:
         """
-        row, col, plr = 2, 2, 1
-        gp.place_piece(self.board, row, col, plr, self.n_rows, self.n_cols)
+        row, col, self.plr = 2, 2, 1
+        gp.place_piece(self.board, row, col, self.plr, self.n_rows, self.n_cols)
         board_after_1st_piece_placed = self.board.copy()
         with self.assertRaises(ValueError):
-            gp.place_piece(self.board, row, col, plr, self.n_rows, self.n_cols)
+            gp.place_piece(self.board, row, col, self.plr, self.n_rows, self.n_cols)
         self.assertIsNone(nptest.assert_array_equal(self.board, board_after_1st_piece_placed))
 
     def test_outside_board(self):
@@ -42,9 +43,9 @@ class TestPiecePlacement(unittest.TestCase):
         place a piece outside of board dimensions.
         :return:
         """
-        row, col, plr = self.n_rows, 2, 1
+        row, col, self.plr = self.n_rows, 2, 1
         with self.assertRaises(IndexError):
-            gp.place_piece(self.board, row, col, plr, self.n_rows, self.n_cols)
+            gp.place_piece(self.board, row, col, self.plr, self.n_rows, self.n_cols)
         self.assertIsNone(nptest.assert_array_equal(self.board, self.test_board))
 
     def test_negative_indices(self):
@@ -53,9 +54,9 @@ class TestPiecePlacement(unittest.TestCase):
         place a piece in negative indices.
         :return:
         """
-        row, col, plr = -1, -3, 1
+        row, col, self.plr = -1, -3, 1
         with self.assertRaises(ValueError):
-            gp.place_piece(self.board, row, col, plr, self.n_rows, self.n_cols)
+            gp.place_piece(self.board, row, col, self.plr, self.n_rows, self.n_cols)
         self.assertIsNone(nptest.assert_array_equal(self.board, self.test_board))
 
 
