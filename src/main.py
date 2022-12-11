6f59956f1075f8f295d2f21c5141c0c035175a63
jrostek <joanna.rostek@etu.univ-nantes.fr>
2022-12-11 23:10:46 +0100

    starting to include the look

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 """API functions for running the game."""
 from fastapi import FastAPI
 from src.gameplay import play_game
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+with open(Path("templates", "index.html")) as f:
+    site = f.read()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory=Path("static")), name="static")
+app.mount("/templates", StaticFiles(directory=Path("templates")), name="templates")
+# templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 async def root():
     """Root of the app."""
-    play_game()
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    """Who knows."""
-    return {"message": f"Hello {name}"}
+    # play_game()
+    replace = site.replace(r"nothing here yet...", "it has worked (somewhat)")
+    return HTMLResponse(replace)
